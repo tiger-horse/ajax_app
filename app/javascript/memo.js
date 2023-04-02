0e35af6d74094ffea1ceeb49a9137158a2d595a6
横尾和馬 <yokookazuma@yokookazumanoMacBook-Air.local>
2023-04-02 20:37:38 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,21 @@
+
+
+
+const buildHTML = (XHR) => {
+  // レスポンスの中から投稿されたメモの情報を抽出し、変数itemに格納
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   // idのsabmitを取得して代入
   const submit = document.getElementById("submit");
@@ -15,7 +33,24 @@ function post (){
     XHR.open("POST", "/posts", true);
     // リクエスト時のデータフォーマットの指定
     XHR.responseType = "json";
+    // リクエストの送信
     XHR.send(formData);
+    // リクエストが送信が成功したら
+    XHR.onload = () => {
+      // リクエストに失敗したとき
+      if (XHR.status != 200) {
+        // エラー文が出る
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      // フォームのidを取得する
+      const formText = document.getElementById("content");
+      // insertAdjacentHTMLメソッドの第一引数にafterendを指定することで、変数listに格納された要素の直後に生成したHTMLを挿入しています
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      // 入力フォームを空にする。
+      formText.value = "";
+    };
   });
 };
 
